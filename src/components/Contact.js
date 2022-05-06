5f3841ec893e5f5e8c6ba59ca14f32bdd95951e4
Chris Chun <ichrischun@gmail.com>
2022-05-06 14:11:19 -0400

    finished emailjs integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_afmus93',
+        'template_d3hjq36',
+        form.current,
+        'bParxth-cIkAYOA0P'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-black flex justify-center items-center p-4"
+    >
+      <form
+        className="flex flex-col max-w-[600px] w-full"
+        ref={form}
+        onSubmit={sendEmail}
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bond inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">Submit Form Below</p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button
+          type="submit"
+          className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
