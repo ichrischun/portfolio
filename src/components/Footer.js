30b6ec7467b1900750657d7bcf82fde7303e6b39
Chris Chun <ichrischun@gmail.com>
2022-05-09 15:48:04 -0400

    added resume and email feature

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-[#353535] text-[#FFFFFF] text-center py-5">
+      <div>@ichrischun</div>
+      <div>Copyright © 2022</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
+import Resume from '../assets/Chris_Chun_Resume.pdf';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -101,7 +102,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/ichrischun"
             >
               Github <FaGithub size={30} />
             </a>
@@ -109,7 +110,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="mailto:ichrischun@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
@@ -117,7 +118,8 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href={Resume}
+              download="Chris_Chun_Resume"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
