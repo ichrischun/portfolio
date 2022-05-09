80b436f6b8c04430582f3de3727c16b5a59404aa
Chris Chun <ichrischun@gmail.com>
2022-05-09 19:28:48 -0400

    added pawgwarts

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-import Placeholder from '../assets/singapore.jpg';
+import Pawgwarts from '../assets/Pawgwarts.png';
 
 const Projects = () => {
-  // const navigate = useNavigate();
   return (
     <div
       name="projects"
@@ -20,7 +18,31 @@ const Projects = () => {
           <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 w-[450px] h-[300px] ">
             <img
               className="w-full h-full mx-auto"
-              src={Placeholder}
+              src={Pawgwarts}
+              alt="JSLogo"
+            />
+            <div className="bg-[#D9D9D9] text-[#284B63] py-1">
+              <p className="my-1 text-2xl">Pawgwarts</p>
+              <p className="my-1 mx-3">
+                A harry-potter themed, dog-training mobile application with a
+                social-media component. Users can select specific trainings for
+                their dogs with step-by-step instructions and share their
+                accomplishments via a social media feed, which consist of a
+                weekly challenge to take on.
+              </p>
+              <a href="https://github.com/TheTimbs/Pawgwarts">
+                <button className="text-center rounded-lg px-4 py-1 m-1 bg-[#353535] text-[#FFFFFF]">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+          {/* card */}
+          {/* card */}
+          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 w-[450px] h-[300px]">
+            <img
+              className="w-full h-full mx-auto"
+              src={Pawgwarts}
               alt="JSLogo"
             />
             <div className="bg-[#D9D9D9] text-[#284B63] py-1">
@@ -38,25 +60,7 @@ const Projects = () => {
           <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 w-[450px] h-[300px]">
             <img
               className="w-full h-full mx-auto"
-              src={Placeholder}
-              alt="JSLogo"
-            />
-            <div className="bg-[#D9D9D9] text-[#284B63] py-1">
-              <p className="my-1 text-2xl">Title</p>
-              <p className="my-1">Description</p>
-              <a href="https://www.linkedin.com/in/ichrischun/">
-                <button className="text-center rounded-lg px-4 py-1 m-1 bg-[#353535] text-[#FFFFFF]">
-                  Code
-                </button>
-              </a>
-            </div>
-          </div>
-          {/* card */}
-          {/* card */}
-          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 w-[450px] h-[300px]">
-            <img
-              className="w-full h-full mx-auto"
-              src={Placeholder}
+              src={Pawgwarts}
               alt="JSLogo"
             />
             <div className="bg-[#D9D9D9] text-[#284B63] py-1">
