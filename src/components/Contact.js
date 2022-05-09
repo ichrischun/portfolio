3a374988b8cb0196e0d3cdf78eca404eeccb2a8a
Chris Chun <ichrischun@gmail.com>
2022-05-09 15:25:16 -0400

    worked on skills and contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,33 +36,37 @@ const Contact = () => {
         ref={form}
         onSubmit={sendEmail}
       >
-        <div className="pb-8">
-          <p className="text-4xl font-bond inline border-b-4 border-pink-600 text-gray-300">
-            Contact
+        <div className="pb-3">
+          <p className="text-4xl font-bond inline border-b-4 border-[#D9D9D9] text-[#FFFFFF]">
+            Contact Me
           </p>
-          <p className="text-gray-300 py-4">Submit Form Below</p>
+          <p className="text-[#FFFFFF] pt-6 pb-4">
+            I'd love to connect! Feel free to reach out about anything -
+            employment, networking, or collaborating!{' '}
+          </p>
+          <p className="text-[#FFFFFF] pt-5">Submit Form Below:</p>
         </div>
         <input
-          className="bg-[#ccd6f6] p-2"
+          className="bg-[#D9D9D9] p-2"
           type="text"
           placeholder="Name"
           name="name"
         />
         <input
-          className="my-4 p-2 bg-[#ccd6f6]"
+          className="my-4 p-2 bg-[#D9D9D9]"
           type="email"
           placeholder="Email"
           name="email"
         />
         <textarea
-          className="bg-[#ccd6f6] p-2"
+          className="bg-[#D9D9D9] p-2"
           name="message"
           rows="10"
           placeholder="Message"
         ></textarea>
         <button
           type="submit"
-          className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
+          className="text-white border-2 hover:bg-[#3C6E71] hover:border-[#FFFFFF] px-4 py-3 my-8 mx-auto flex items-center"
         >
           Submit
         </button>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,39 @@
 import React from 'react';
-import LinkedIn from '../assets/LinkedIn.png';
+import JavaScript from '../assets/JavaScript.png';
+import React_Logo from '../assets/React_Logo.png';
+import Postgres from '../assets/Postgres.png';
+import Nodejs from '../assets/Nodejs.png';
+import Firebase from '../assets/Firebase.png';
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#D9D9D9] text-gray-300">
+    <div name="skills" className="w-full h-screen bg-[#D9D9D9] text-[#284B63]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-4xl text-gray-300 py-4 inline border-b-4 border-pink-600">
+          <p className="text-4xl py-4 inline border-b-4 border-[#3C6E71]">
             Skills
           </p>
         </div>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-black hover:scale-110 duration-300">
-            <img className="w-20 mx-auto" src={LinkedIn} alt="LinkedIn Pic" />
-            <p className="my-4">Fill Me In</p>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 my-4">
+          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 pt-5">
+            <img className="w-20 mx-auto" src={JavaScript} alt="JSLogo" />
+            <p className="my-4 text-[#FFFFFF]">JavaScript</p>
           </div>
-          <div className="shadow-md shadow-black hover:scale-110 duration-300">
-            <img className="w-20 mx-auto" src={LinkedIn} alt="LinkedIn Pic" />
-            <p className="my-4">Fill Me In</p>
+          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 pt-5">
+            <img className="w-20 mx-auto" src={React_Logo} alt="ReactLogo" />
+            <p className="my-4 text-[#FFFFFF]">React</p>
           </div>
-          <div className="shadow-md shadow-black hover:scale-110 duration-300">
-            <img className="w-20 mx-auto" src={LinkedIn} alt="LinkedIn Pic" />
-            <p className="my-4">Fill Me In</p>
+          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 pt-5">
+            <img className="w-20 mx-auto" src={Postgres} alt="PostgresLogo" />
+            <p className="my-4 text-[#FFFFFF]">PostgresSQL</p>
           </div>
-          <div className="shadow-md shadow-black hover:scale-110 duration-300">
-            <img className="w-20 mx-auto" src={LinkedIn} alt="LinkedIn Pic" />
-            <p className="my-4">Fill Me In</p>
+          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 pt-5">
+            <img className="w-20 mx-auto" src={Nodejs} alt="NodeLogo" />
+            <p className="my-4 text-[#FFFFFF]">Node.js</p>
+          </div>
+          <div className="shadow-md bg-[#353535] shadow-[#353535] hover:scale-110 duration-300 pt-5">
+            <img className="w-20 mx-auto" src={Firebase} alt="FirebaseLogo" />
+            <p className="my-4 text-[#FFFFFF]">Google Firebase</p>
           </div>
         </div>
       </div>
